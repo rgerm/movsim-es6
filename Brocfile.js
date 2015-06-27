357cd406506c6323cb42ef01d9ea8e01eb68c44b
rgerm <germ@ralphgerm.de>
2015-06-27 23:53:39 +0200

    Switched buildtool from gulp to broccoli

---
+++ Brocfile.js
@@ -0,0 +1,24 @@
+var browserify = require('broccoli-browserify');
+var merge = require('broccoli-merge-trees');
+var esTranspiler = require('broccoli-babel-transpiler');
+var funnel = require('broccoli-funnel');
+
+var htmlFiles = funnel('src', {
+	files: ['index.html']
+});
+
+var cssFiles = funnel('src/css', {
+	destDir: 'css'
+});
+
+var resFiles = funnel('src/img', {
+	destDir: 'img'
+});
+
+var scriptTree = esTranspiler('src', {});
+var jsFile = browserify(scriptTree, {
+	entries: ['./js/main.js'],
+	outputFile: 'application.js'
+});
+
+module.exports = merge([htmlFiles, cssFiles, resFiles, jsFile]);
